a681b478727648733c74b318438b44fbef35044b
mugi-uno <mugi.uno@gmail.com>
2019-05-22 22:35:28 +0900

    テストコードを追加

---
+++ spec/snapshot.spec.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+describe("TODOアプリ", () => {
+  beforeEach(async () => {
+    await page.goto("file://" + path.resolve(__dirname, "../index.html"));
+  });
+
+  it("初期表示", async () => {
+    expect(await page.content()).toMatchSnapshot();
+  });
+
+  it("タスクの追加", async () => {
+    await page.click("#addTodo");
+    await page.click("#addTodo");
+    expect(await page.content()).toMatchSnapshot();
+  });
+
+  it("タスクの入力", async () => {
+    await page.click("#addTodo");
+    await page.click("#addTodo");
+    await page.type(".todo:nth-child(1) input", "サンプルタスク1");
+    await page.type(".todo:nth-child(2) input", "サンプルタスク2");
+    expect(await page.content()).toMatchSnapshot();
+  });
+
+  it("タスクの削除", async () => {
+    await page.click("#addTodo");
+    await page.click("#addTodo");
+    await page.type(".todo:nth-child(1) input", "サンプルタスク1");
+    await page.type(".todo:nth-child(2) input", "サンプルタスク2");
+    await page.click(".todo:nth-child(1) .delete");
+    await page.click(".todo:nth-child(1) .delete");
+    expect(await page.content()).toMatchSnapshot();
+  });
+});
---
+++ spec/visual.spec.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const { toMatchImageSnapshot } = require("jest-image-snapshot");
+expect.extend({ toMatchImageSnapshot });
+
+describe("TODOアプリ", () => {
+  beforeEach(async () => {
+    await page.goto("file://" + path.resolve(__dirname, "../index.html"));
+  });
+
+  it("初期表示", async () => {
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+
+  it("タスクの追加", async () => {
+    await page.click("#addTodo");
+    await page.click("#addTodo");
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+
+  it("タスクの入力", async () => {
+    await page.click("#addTodo");
+    await page.click("#addTodo");
+    await page.type(".todo:nth-child(1) input", "サンプルタスク1");
+    await page.type(".todo:nth-child(2) input", "サンプルタスク2");
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+
+  it("タスクの削除", async () => {
+    await page.click("#addTodo");
+    await page.click("#addTodo");
+    await page.type(".todo:nth-child(1) input", "サンプルタスク1");
+    await page.type(".todo:nth-child(2) input", "サンプルタスク2");
+    await page.click(".todo:nth-child(1) .delete");
+    await page.click(".todo:nth-child(1) .delete");
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+});
