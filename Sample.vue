9f2fc744504811c687dad761e5d8099169ce5f9c
mugi-uno <mugi.uno@gmail.com>
2019-05-30 22:54:17 +0900

    Vue.js - SFCのセットアップ

---
+++ Sample.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>sample</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      str: "abc" as string
+    }
+  }
+})
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
 module.exports = {
   mode: "development",
   devtool: "source-map",
   entry: "./js/script.ts",
   resolve: {
-    extensions: [".ts", ".js"]
+    extensions: [".ts", ".js", ".vue"]
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: { loader: "ts-loader" }
+        use: {
+          loader: "ts-loader",
+          options: { appendTsSuffixTo: [/\.vue$/] }
+        }
+      },
+      {
+        test: /\.vue$/,
+        use: {
+          loader: "vue-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
       }
     ]
-  }
+  },
+  plugins: [new VueLoaderPlugin()]
 };
